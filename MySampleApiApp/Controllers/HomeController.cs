cfdb40ca46dfde752d1d68a6749838f5679d6e5a
sasanka1983 <39948966+sasanka1983@users.noreply.github.com>
2019-04-25 22:18:58 +0530

    Initial commit

---
+++ MySampleApiApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MySampleApiApp.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetResult([FromUri] string id)
+        {
+            return Ok(new { Id = 2, Name = "Test" });
+        }
+
+        [HttpPost]
+        public IHttpActionResult SaveResult(string name)
+        {
+            return Ok(new { Id = 4, Name = "saved "+name });
+        }
+    }
+}
